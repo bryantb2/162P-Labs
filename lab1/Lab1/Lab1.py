b56905cc0af4a57fcf34a872b927b5781ed04da0
Blake <bryantb2@my.lanecc.edu>
2020-04-06 19:16:19 -0700

    finished testing for lab 1

---
+++ lab1/Lab1/Lab1.py
@@ -0,0 +1,54 @@
+"""
+    Check default cases
+        then    
+    Check standard cases
+"""
+def is_first_power_of_second(a,b):
+    is_power = False;
+    if (a % b == 0 and ((a/b) % b) == 0) :
+        is_power = True;
+    # b raised to 0 equals a
+    # b raised to 1 equals a
+    elif (b**0 == a or b**1 == a) :
+        is_power = True;
+    print('Is ' + str(a) + ' power of ' + str(b) + ': ' + str(is_power));
+    return is_power;
+
+# Setup Tests
+def test_power_function():
+    testData = [
+        {
+            "a": 5,
+            "b": 5
+        },
+        {
+            "a": 25,
+            "b": 5
+        },
+        {
+            "a": 125,
+            "b": 25
+        },
+        {
+            "a": 1,
+            "b": 13
+        },
+        {
+            "a": 64,
+            "b": 4
+        },
+        {
+            "a": 27,
+            "b": 4
+        },
+        {
+            "a": 256,
+            "b": 2
+        }
+    ];
+    for dataPair in testData:
+        is_first_power_of_second(dataPair["a"],dataPair["b"]);
+
+
+# Run Test
+test_power_function();
