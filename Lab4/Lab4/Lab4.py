e26ac9b760c3437642298ebc0f54bb739f28b67c
Blake <bryantb2@my.lanecc.edu>
2020-04-28 21:55:27 -0700

    finished debugging UI, grid, and exception handling errors

--- Lab4/Lab4/Lab4.py
+++ Lab4/Lab4/Lab4.py
@@ -1,3 +1,6 @@
+# library imports
+import random;
+
 # Board variables
 trap_symbol = 'T';
 treasure_symbol = 'X';
@@ -28,13 +31,13 @@ def translate_move_to_indexes(dungeon, current_player_pos, move_type):
     row_pos = current_player_pos["row"];
     col_pos = current_player_pos["col"];
     move_dictionary = { "row": 0, "col": 0 };
-    if(move_left):
+    if(move_left == move_type):
         move_dictionary["row"] = row_pos;
         move_dictionary["col"] = col_pos - 1;
-    elif(move_right):
+    elif(move_right == move_type):
         move_dictionary["row"] = row_pos;
         move_dictionary["col"] = col_pos + 1;
-    elif(move_up):
+    elif(move_up == move_type):
         move_dictionary["row"] = row_pos - 1;
         move_dictionary["col"] = col_pos;
     else:
@@ -105,13 +108,13 @@ def return_inbound_moves(dungeon, current_position_indexes):
     moved_up = translate_move_to_indexes(dungeon, current_position_indexes, move_up);
     moved_down = translate_move_to_indexes(dungeon, current_position_indexes, move_down);
     # test if moves are within bounds
-    if(move_out_of_bounds(moved_left)):
+    if(move_out_of_bounds(moved_left) == False):
         validate_move_arr.append(move_left);
-    elif(move_out_of_bounds(moved_right)):
+    if(move_out_of_bounds(moved_right) == False):
         validate_move_arr.append(move_right);
-    elif(move_out_of_bounds(moved_up)):
+    if(move_out_of_bounds(moved_up) == False):
         validate_move_arr.append(move_up);
-    elif(move_out_of_bounds(moved_down)):
+    if(move_out_of_bounds(moved_down) == False):
         validate_move_arr.append(move_down);
     # return move list
     return validate_move_arr;
@@ -121,8 +124,8 @@ def prompt_move(prompt_message):
 
 def print_inbound_moves_UI_string(move_arr):
     UI_string = '';
-    for index in range(move_arr.count()):
-        UI_string += move_arr[index] + (', ' if index != move_arr.count() - 1 else '');
+    for index in range(len(move_arr)):
+        UI_string += move_arr[index] + (', ' if index != len(move_arr) - 1 else '');
     print('Move not within game bounds. Only the following moves are valid, given your position: ' + UI_string);
 
 def print_available_move_types():
@@ -154,22 +157,23 @@ def handle_move(dungeon, move_type):
             # check for win loss conditions
             if(check_if_collides(dungeon, move_indexes, trap_symbol)):
                 # show board
-                print_dungeon_board(dungeon);
+                new_dungeon = make_player_move(dungeon, current_pos, move_indexes);
+                print_dungeon_board(new_dungeon);
                 print('You moved onto a trap! Game over.');
             elif(check_if_collides(dungeon, move_indexes, treasure_symbol)):
                 # show board
-                print_dungeon_board(dungeon);
+                new_dungeon = make_player_move(dungeon, current_pos, move_indexes);
+                print_dungeon_board(new_dungeon);
                 print('You got to the treasure. Congrats, you have won!');
             else:
                 # make move
-                new_dungeon = make_player_move(dungeon, current_pos, move_indexes);
                 # print move
                 # show board
+                new_dungeon = make_player_move(dungeon, current_pos, move_indexes);
                 print_move(move_type);
                 print_dungeon_board(new_dungeon);
-                move_input = prompt_move('Please enter a valid move character: ');
                 # Call handle game and pass in new move value
-                handle_move(new_dungeon, move_input);
+                handle_move(new_dungeon, prompt_move('Please enter a valid move character: '));
         else: 
             # Print errors and prompt for new move value
             print_inbound_moves_UI_string( return_inbound_moves(dungeon, current_pos) );
@@ -189,15 +193,15 @@ def generate_dungeon():
     # Create array based on dimensions at top of file
     # Generate set number of traps
     # Place treasure and player
-    dungeon_board = build_board;
     base_dungeon = fill_dungeon(build_board(board_col_number), board_row_number);
+    return base_dungeon;
 
 def fill_dungeon(dungeon_structure, height):
     # height is required to generate the proper number of rows in the dungeon structure
-    for col in dungeon_structure:
+    for col_num in range(len(dungeon_structure)):
         height_num = 0;
-        while(i < height):
-            col.append(empty_square_symbol);
+        for height_num in range(height):
+            dungeon_structure[col_num].append(empty_square_symbol);
     return dungeon_structure;
 
 def build_board(width):
@@ -222,7 +226,7 @@ def add_traps_to_blank_board(blank_dungeon, trap_number):
         while trap_not_set == True:
             random_row = random.randrange(0, board_row_number, 1);
             random_col = random.randrange(0, board_col_number, 1);
-            if(blank_dungeon[random_col][randomrandom_row] != trap_symbol):
+            if(blank_dungeon[random_col][random_row] != trap_symbol):
                 trap_not_set = False;
                 blank_dungeon[random_col][random_row] = trap_symbol;
         current_trap+=1;
@@ -241,7 +245,7 @@ def add_player_and_treasure(trap_fill_dungeon):
     while player_created == False:
         random_row = random.randrange(0, board_row_number, 1);
         random_col = random.randrange(0, board_col_number, 1);
-        target_square = trap_filled_dungeon[random_col][random_row];
+        target_square = trap_fill_dungeon[random_col][random_row];
         if(trap_fill_dungeon[random_col][random_row] != trap_symbol):
             player_created = True;
             trap_fill_dungeon[random_col][random_row] = player_symbol;
@@ -249,28 +253,33 @@ def add_player_and_treasure(trap_fill_dungeon):
     while treasure_created == False:
         random_row = random.randrange(0, board_row_number, 1);
         random_col = random.randrange(0, board_col_number, 1);
-        target_square = trap_filled_dungeon[random_col][random_row];
+        target_square = trap_fill_dungeon[random_col][random_row];
         if(trap_fill_dungeon[random_col][random_row] != trap_symbol and trap_fill_dungeon[random_col][random_row] != player_symbol):
             treasure_created = True;
-            trap_fill_dungeon[random_col][random_row] = player_symbol
+            trap_fill_dungeon[random_col][random_row] = treasure_symbol;
     # return board
     return trap_fill_dungeon;
-
 
 def reset_game():
     # generate dungeon
     # fill dungeon with entities
+    # print instructions
+    # print board
+    # start game
     dungeon = generate_dungeon();
     board_with_players_treasure = add_player_and_treasure(add_traps_to_blank_board(dungeon, 4));
 
-    # print instructions
-    print('The dungeon crawler game involves a computer-generated board filled with traps, marked as ' + trap_symbol + ", that must be avoided as you " +
-    " the player, marked as " + player_symbol + ", traverse the board towards the treasure, marked as " + treasure_symbol + "." + " To win, you must safety move left, right, up, or down to the treasure, without touching the traps. " +
-    "Note that the empty squares, marked as " + empty_square_symbol + " are safe to come in contact with, and should be the only squares you touch.");
-    # print board
+    print("------------- Instructions -------------");
+    print('The dungeon crawler game involves a computer-generated board filled with traps, marked as ' + trap_symbol + ", that must be avoided as you ");
+    print("the player, marked as " + player_symbol + ", " + "traverse the board towards the treasure, marked as " + treasure_symbol + ".");
+    print("To win, you must safety move left, right, up, or down to the treasure, without touching the traps.")
+    print("Note that the empty squares, marked as " + empty_square_symbol + " are safe to come in contact with, and should be the only squares you touch.");
+    print();
+    print("------------- Begin Game -------------");
+    print();
     print_dungeon_board(board_with_players_treasure);
-    # start game
-    handle_move(board_with_players_treasure, prompt_move('Please make your first move. Use one of the following characters to traverse the board: L, R, U, or D '));
+    print();
+    handle_move(board_with_players_treasure, prompt_move('Please make your first move. Use L, R, U, or D to traverse the board: '));
 
 
 # testing functions
